0023b825448cf1f6b2627ff2a81a0b55ed9d1f17
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-06-25 22:48:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+video = "";
+status = "";
+alarm = "";
+objects = [];
+
+function preload(){
+    alarm = loadSound("alarm.mp3")
+}
+function setup(){
+    canvas = createCanvas(380,350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector("cocossd",model_loaded);
+    document.getElementById("detection").innerHTML = " : Detecting Object";
+}
+function draw(){
+    image(video,0,0,380,350);
+    if(status != ""){
+        objectDetector.detect(video,got_results);
+        document.getElementById("detection").innerHTML = " : Object Detected";
+        document.getElementById("found").innerHTML = "<h3 class='button' >Baby Found</h3>";
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        for(i = 0 ; i < objects.length ; i++ ){
+            fill(r,g,b);
+            stroke(r,g,b);
+            percentage = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percentage + "%",objects[i].x,objects[i].y - 10);
+            textSize(20)
+            noFill();
+            rect(objects[i].x - 50,objects[i].y - 10,objects[i].width - 200,objects[i].height - 150)
+        }
+    }
+    if(status = ""){
+        document.getElementById("found").innerHTML = "<h3 class='button' >Baby Not Found</h3>";
+        alarm.play();
+    }
+}
+function model_loaded(){
+    console.log("Model is loaded!");
+    status = true;
+}
+function got_results(error,results){
+    if(error){
+        console.error("Error");
+    }
+    else if(results){
+        objects = results;
+    }
+}
